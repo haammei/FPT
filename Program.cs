553ad21f50522e1e787e80a87e534d5de4f4ed3e
haammei <109502910+haammei@users.noreply.github.com>
2024-05-06 15:28:21 +0700

    Add files via upload
    Complete web interface

---
+++ Program.cs
@@ -0,0 +1,39 @@
+using FPT_LongChau_Pharmacies.Repositori;
+using Microsoft.EntityFrameworkCore;
+
+namespace FPT_LongChau_Pharmacies
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            // Connect Db
+            builder.Services.AddDbContext<DataContext>(Options =>
+            {
+                Options.UseSqlServer(builder.Configuration["ConnectionStrings:ConnectedDb"]);
+            });
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.Run();
+        }
+    }
+}
